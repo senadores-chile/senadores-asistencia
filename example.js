fef1a3cf217be5beee2cf4faa3bdfd40b89de61b
Yerko Palma <yerko.palma@usach.cl>
2016-11-27 22:50:04 +0000

    [skip ci] started examples

--- example.js
+++ example.js
@@ -10,11 +10,17 @@ asistencia('Allamand', { periodo: 486 })
     Asistencia a comisiones de los senadores del partido socialista
     durante el año 2011
  */
+asistencia({ partido: 'P.S.' }, { tipo: 'comision' })
+    .then(result => { console.log(result) })
 
 /*
     Promedio de inasistencias de los senadores de Renovación Nacional
     para el periodo actual
  */
+asistencia({ partido: 'R.N.' }, { tipo: 'sala' })
+    .then(result => {
+
+    })
 
 /*
     Detalle de todas las sesiones de sala a las que falto el senador
